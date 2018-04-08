d7a017df75fe6099b897e333981ab19f993e13fa
PC-20170726NIEA\Administrator <18321941335@163.com>
2018-04-08 14:13:16 +0800

    hahahahahaha

--- public/js/detail/detail.js
+++ public/js/detail/detail.js
@@ -11,4 +11,21 @@ $(function() {
         mouseWheel: true,
         scrollbars: true
     });
+    //    侧边栏
+    $(".ce-btns").mouseover(function(){
+        $(this).css({color:"#a55c3f"}).siblings().css({});
+        $(".ce-btns .Alert").eq($(this).index()).show();
+    });
+    $(".ce-btns").mouseleave(function(){
+        $(this).css({background:"",color:""});
+        $(".ce-btns .Alert").eq($(this).index()).hide();
+    });
+
+    $(document).ready(function(){
+        var h =$(document.body).outerHeight(true);
+        if(h>700){
+            $('.header').css({'background':'#fff','border-bottom':'1px solid rgba(0,0,0,0.1'})
+        }
+    });
+})
 })
--- public/js/news/news.js
+++ public/js/news/news.js
@@ -1,5 +1,5 @@
 $(function() {
-    // nav 
+    // nav
     $(".nav_btn").on('click', function(event) {
         event.preventDefault();
         /* Act on the event */
@@ -93,5 +93,20 @@ $(function() {
     })
 
 
+//    侧边栏
+    $(".ce-btns").mouseover(function(){
+        $(this).css({color:"#a55c3f"}).siblings().css({});
+        $(".ce-btns .Alert").eq($(this).index()).show();
+    });
+    $(".ce-btns").mouseleave(function(){
+        $(this).css({background:"",color:""});
+        $(".ce-btns .Alert").eq($(this).index()).hide();
+    });
 
+    $(document).ready(function(){
+        var h =$(document.body).outerHeight(true);
+        if(h>700){
+            $('.header').css({'background':'#fff','border-bottom':'1px solid rgba(0,0,0,0.1'})
+        }
+    });
 })
